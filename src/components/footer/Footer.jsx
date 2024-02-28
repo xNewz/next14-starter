c5ada47b13a9ec6657b3353cadb6f40e9222135f
xNewz <63030068@kmitl.ac.th>
2024-02-28 10:37:22 +0700

    add: style home and about pages

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,14 @@
+import styles from "./footer.module.css";
+
 const Footer = () => {
-    return (
-        <div>Footer</div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Logo</div>
+      <div className={styles.text}>
+        Pargorn © All rights reserved.
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
